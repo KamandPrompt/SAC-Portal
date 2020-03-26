e330df725ade8aeb9f7b172b2f68fd6284f1bc30
naveensaigit <56525288+naveensaigit@users.noreply.github.com>
2020-03-26 14:43:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
-import flask
+from flask import url_for,render_template,redirect,Flask,flash
 from forms import LoginForm
-app=flask.Flask(__name__)
+
+app=Flask(__name__)
 app.config['SECRET_KEY']='c828b6ff21f45063fd7860e5c1b1d233'
 
 @app.route('/')
 def home():
-    return flask.render_template('index.html')
+    return render_template('index.html')
 
 @app.route("/login",methods=['GET','POST'])
 def Login():
     form=LoginForm()
     if form.validate_on_submit():
         if form.email.data[1:6].isdecimal() and form.email.data[6:]=='@students.iitmandi.ac.in' :
-            flask.flash('You have logged-in successfully','success')
-            return flask.redirect(flask.url_for('home'))
+            flash('You have logged-in successfully','success')
+            return redirect(url_for('home'))
         else:
-            flask.flash('Invalid Email/Password','danger')
-    return flask.render_template('login.html',form=form)
+            flash('Invalid Email/Password','message')
+    return render_template('login.html',title='Login | SAC Portal, IIT Mandi',form=form)
 
 if __name__=="__main__":
     app.run(debug=True)
