c4cad1c41dcfee8da802bd38cde38a9511fc1bb3
naveensaigit <56525288+naveensaigit@users.noreply.github.com>
2020-03-26 21:14:43 +0530

    Created Jinja Layout and Login Page

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
-from flask import Flask,render_template
+from flask import url_for,render_template,redirect,Flask,flash
+from forms import LoginForm
 
-app = Flask(__name__)
-
+app=Flask(__name__,static_url_path='/public')
+app.config['SECRET_KEY']='c828b6ff21f45063fd7860e5c1b1d233'
 
 @app.route('/')
-def hello_world():
-    return render_template('basic.html')
+def home():
+    return render_template('index.html')
 
+@app.route("/login",methods=['GET','POST'])
+def Login():
+    form=LoginForm()
+    if form.validate_on_submit():
+        if form.email.data[1:6].isdecimal() and form.email.data[6:]=='@students.iitmandi.ac.in' :
+            flash('You have logged-in successfully!')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Invalid Email/Password')
+    return render_template('login.html',title='Login | SAC Portal, IIT Mandi',form=form)
 
-if __name__ == '__main__':
-    app.run()
+if __name__=="__main__":
+    app.run(debug=True)
