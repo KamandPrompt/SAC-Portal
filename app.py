7fa2eacc1a2adce3a89d3e5dba7663c58aa3fc9d
Solai-Adithya <solaiadithya001@gmail.com>
2020-04-02 22:44:28 +0530

    Added API to add a coordinator

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import url_for,render_template,redirect,Flask,flash
+from flask import url_for,render_template,redirect,Flask,flash,session
 from forms import LoginForm
 from Make_Tables.mysqlconnect import mydb, mycursor, create_insert_statement   #Imported the mysqlconnect.py file from Make_tables folder
 
@@ -20,5 +20,24 @@ def Login():
             flash('Login Unsuccessful. Invalid Email/Password')
     return render_template('login.html',title='Login | SAC Portal, IIT Mandi',form=form)
 
+@app.route("/AddCoordinator",methods=['GET','POST'])
+def AddCoordinator():
+    email=session['email'][0:6]
+    isadmin=mycursor.execute('SELECT EXISTS(SELECT * from Coordinators WHERE userID="'+email+'");')
+    if(isadmin):
+        data={} #The possible input method can be decided after designing the frontend for the page to add coordinators
+        userID=data['userID']
+        clubID=data['clubID']
+        try:
+            mycursor.execute('INSERT into Coordinators VALUES("'+clubID+'","'+userID+'");')
+            mydb.commit()
+            return 'Data inserted successfully'
+        except:
+            return 'Error Inserting Data'
+
+    else:
+        return 'Sorry! You are not an Admin'
+
 if __name__=="__main__":
     app.run(debug=True)
+
