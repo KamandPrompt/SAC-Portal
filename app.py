6b4e0fe1673991f2b0bc1c7c4297c05c44f46b72
Signior-X <b19188@students.iitmandi.ac.in>
2020-04-11 21:04:45 +0530

    Made some minor chnages

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import url_for,render_template,redirect,Flask,flash, session
+from flask import url_for,render_template,redirect,Flask,flash, session, request
 from forms import LoginForm
 from Make_Tables.mysqlconnect import mydb, mycursor, create_insert_statement, sqlerror   #Imported the mysqlconnect.py file from Make_tables folder
 
@@ -7,6 +7,7 @@ app.config['SECRET_KEY']='c828b6ff21f45063fd7860e5c1b1d233'
 
 @app.route('/')
 def home():
+    flash("Yes")
     return render_template('index.html')
 
 @app.route("/login",methods=['GET','POST'])
@@ -20,12 +21,28 @@ def Login():
             flash('Login Unsuccessful. Invalid Email/Password')
     return render_template('login.html',title='Login | SAC Portal, IIT Mandi',form=form)
 
-#API to turn a normal user into an admin
-@app.route("/makeadmin/<uid>", methods=['POST'])
-def make_admin(uid):
-    #First check if session is actually logged in
 
-    if session['logged_in'] == True:
+# API to turn a normal user into an admin
+# Send the data in JSON Format
+# data = {
+#   "userID": "b19188"
+# }
+@app.route("/makeadmin", methods=['POST'])
+def make_admin():
+
+    #Get the data
+    data = request.get_json()
+    uid = data['userID']
+    #print(uid)
+
+    #First check if session is actually logged in
+    isLogin = False
+    try:
+        isLogin = session['logged_in']
+    except KeyError as err:   #So that error will not come if the session is not defined
+        return 'Error came: Key Error: '+str(err)
+
+    if isLogin == True:
 
         #Now check if the loggined user is admin or not
         isadmin=session['admin']
@@ -57,14 +74,30 @@ def make_admin(uid):
             return error
     else:
         error = "You are not logged in, Please Login!"
-        return render_template('login.html', error = error, form = form)  #API : Login should be ready
+        return 'error'  #API : Login should be ready after that we can use 
+        #return render_template('login.html')
 
 #API to turn an admin into a normal user
-@app.route("/removeadmin/<uid>", methods=['POST'])
+# Send the data in JSON Format
+# data = {
+#   "userID": "b19188"
+# }
+@app.route("/removeadmin", methods=['POST'])
 def remove_admin(uid):
-    #First check if session is actually logged in
 
-    if session['logged_in'] == True:
+    #Get the data
+    data = request.get_json()
+    uid = data['userID']
+    #print(uid)
+
+    #First check if session is actually logged in
+    isLogin = False
+    try:
+        isLogin = session['logged_in']
+    except KeyError as err:   #So that error will not come if the session is not defined
+        return 'Error came: Key_Error: '+str(err)
+
+    if isLogin == True:     
 
         #Now check if the loggined user is admin or not
         isadmin = session['admin']
@@ -96,7 +129,8 @@ def remove_admin(uid):
             return error
     else:
         error = "You are not logged in, Please Login!"
-        return render_template('login.html', error = error, form=form)  #API : Login should be ready
+        return 'error'  #API : Login should be ready after that we can use 
+        #return render_template('login.html')
 
 
 
